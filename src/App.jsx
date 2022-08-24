49244823a227801f2e91930f0c7b211ff7b9986d
syoungee <sunyoungah69@gmail.com>
2022-08-25 00:06:14 +0900

    feature: main 경로 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import IssuePage from './pages/IssuePage';
 import Repos from './pages/Repos';
 import NotFound from './pages/NotFound';
+import Main from './pages/Main';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route>
+          <Route path="/" element={<Main />} />
           <Route path="/repos" element={<Repos />} />
           <Route path="/issues" element={<IssuePage />} />
         </Route>
